895250ee23135645ab0cece976a3fe1baaf19493
Chandler Zhao <zhao_chunxiao@hotmail.com>
2024-06-14 06:44:55 +0800

    update compile script, simulation works on vivado

--- labs/lab1/run/compile.sh
+++ labs/lab1/run/compile.sh
@@ -1,1 +1,5 @@
-xvlog -f file.f -sv -L work --include ../uvm_lib --include ../test ../uvm_lib/my_transaction.sv ../test/my_test.sv ../uvm_lib/my_env.sv ../uvm_lib/my_sequence.sv ../uvm_lib/my_sequence.sv 
+#xvlog -f file.f -sv -L work --include ../uvm_lib --include ../test ../uvm_lib/my_transaction.sv ../test/my_test.sv ../uvm_lib/my_env.sv ../uvm_lib/my_sequence.sv ../uvm_lib/my_sequence.sv 
+xvlog -f file.f -sv -L work 
+xelab --incr -timescale 1ns/1ps -L work -L unisims_ver -L xpm -L unimacro_ver -L secureip -L UVM test
+xsim test -R -testplusarg UVM_TESTNAME=my_test 
+
--- labs/lab1/run/file.f
+++ labs/lab1/run/file.f
@@ -1,4 +1,6 @@
 ../test/test.sv 
+--include ../uvm_lib 
+--include ../test 
 -L uvm 
 -L xilinx_vip 
 --uvm_version  1.2 
